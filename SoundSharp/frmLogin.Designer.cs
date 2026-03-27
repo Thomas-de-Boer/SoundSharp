c07f5458b02e2dc7e67fac1965dfe7f38e7af3cc
Thomas Boer <203590@novacollege.nl>
2026-03-27 10:37:01 +0100

    [ADD] login form

---
+++ SoundSharp/frmLogin.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace SoundSharp
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            lblUsername = new Label();
+            tbxUsername = new TextBox();
+            lblPassword = new Label();
+            tbxPassword = new TextBox();
+            btnSubmit = new Button();
+            lblError = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(367, 136);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(75, 20);
+            lblUsername.TabIndex = 0;
+            lblUsername.Text = "Username";
+            // 
+            // tbxUsername
+            // 
+            tbxUsername.BackColor = Color.Magenta;
+            tbxUsername.Location = new Point(342, 159);
+            tbxUsername.Name = "tbxUsername";
+            tbxUsername.Size = new Size(125, 27);
+            tbxUsername.TabIndex = 1;
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(367, 189);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(70, 20);
+            lblPassword.TabIndex = 2;
+            lblPassword.Text = "Password";
+            // 
+            // tbxPassword
+            // 
+            tbxPassword.BackColor = Color.Cyan;
+            tbxPassword.Location = new Point(342, 212);
+            tbxPassword.Name = "tbxPassword";
+            tbxPassword.Size = new Size(125, 27);
+            tbxPassword.TabIndex = 3;
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.BackColor = Color.Yellow;
+            btnSubmit.Location = new Point(357, 245);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(94, 29);
+            btnSubmit.TabIndex = 4;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = false;
+            // 
+            // lblError
+            // 
+            lblError.AutoSize = true;
+            lblError.Location = new Point(382, 288);
+            lblError.Name = "lblError";
+            lblError.Size = new Size(41, 20);
+            lblError.TabIndex = 5;
+            lblError.Text = "Error";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(324, 426);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(473, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(315, 426);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.InitialImage = (Image)resources.GetObject("pictureBox3.InitialImage");
+            pictureBox3.Location = new Point(342, 12);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(125, 121);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 8;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(342, 323);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(125, 115);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 9;
+            pictureBox4.TabStop = false;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Lime;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblError);
+            Controls.Add(btnSubmit);
+            Controls.Add(tbxPassword);
+            Controls.Add(lblPassword);
+            Controls.Add(tbxUsername);
+            Controls.Add(lblUsername);
+            Name = "frmLogin";
+            Text = "Login";
+            Load += frmLogin_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblUsername;
+        private TextBox tbxUsername;
+        private Label lblPassword;
+        private TextBox tbxPassword;
+        private Button btnSubmit;
+        private Label lblError;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+    }
+}
